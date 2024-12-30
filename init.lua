7e2b08157525ef8f8c035ab4463da52bf2596cf8
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2024-12-30 15:21:21 +0200

    lazy.vim, catppuccin mocha colorscheme, mini.nvim statusline

--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
 print("hello world")
+
+require("config.lazy")
+
+
+
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking (coping) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', {clear = true}),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
