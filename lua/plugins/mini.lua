a2b03d2d4166304bcbe0340f4756e56453112d5e
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2025-01-03 00:54:12 +0200

    mini.icons, telescope initial setup, which key initial

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,12 +1,12 @@
 return {
-  { 
+  {
     'echasnovski/mini.nvim',
     version = false,
     enabled = true,
     config = function()
       local statusline = require 'mini.statusline'
-      statusline.setup {use_icons = true }
+      statusline.setup { use_icons = true }
+      require('mini.icons').setup()
     end,
   },
 }
-
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,22 @@
+return {              -- Useful plugin to show you pending keybinds.
+  'folke/which-key.nvim',
+  event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    require('which-key').setup()
+
+    -- Document existing key chains
+    require('which-key').add {
+      { '<leader>c', name = '[C]ode',      hidden = true },
+      { '<leader>d', name = '[D]ocument',  hidden = true },
+      { '<leader>s', name = '[S]earch' },
+      { '<leader>w', name = '[W]orkspace', hidden = true },
+      { '<leader>t', name = '[T]oggle',    hidden = true },
+      { '<leader>h', name = 'Git [H]unk',  hidden = true },
+      { '<leader>N', name = '[N]eotree',   hidden = true },
+      {
+        { mode = 'v' },
+        { '<leader>h', name = 'Git [H]unk' },
+      },
+    }
+  end,
+}
