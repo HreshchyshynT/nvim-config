40c135f3b87ffc318d6468fa33d2c28bdae89078
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2025-01-06 11:35:23 +0200

    oil.vim and blink for autocompletion

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      delete_to_trash = true,
+    },
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  }
+}
