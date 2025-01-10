fed5f1238f68383ba8d1162122b9fc756450d808
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2025-01-11 00:41:04 +0200

    toggle oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -8,7 +8,16 @@ return {
     },
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     init = function()
-      vim.keymap.set("n", "<leader>O", ":Oil<CR>", { desc = "[O]il" })
+      vim.keymap.set("n", "<leader>O", function()
+        local oil = require("oil")
+        local buf = vim.api.nvim_get_current_buf()
+        local filetype = vim.api.nvim_get_option_value("filetype", { buf = buf })
+        if filetype == "oil" then
+          oil.close()
+        else
+          oil.open()
+        end
+      end, { desc = "[O]il" })
     end,
   },
 }
