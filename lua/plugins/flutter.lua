534346736d23c8357ae85878499dc617a1937243
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2025-01-27 11:36:19 +0200

    filter flutter spammed log line when using scrcpy

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -1,25 +1,37 @@
 return {
-  'akinsho/flutter-tools.nvim',
+  "akinsho/flutter-tools.nvim",
   lazy = false,
   dependencies = {
-    'nvim-lua/plenary.nvim',
+    "nvim-lua/plenary.nvim",
   },
   config = function()
-    local flutter = require 'flutter-tools'
-    flutter.setup {
+    local flutter = require("flutter-tools")
+    flutter.setup({
       fvm = true,
-    }
-    local telescope = require 'telescope'
-    local whichKey = require 'which-key'
+      dev_log = {
+        filter = function(line)
+          return not line:match(
+            "handleWindowFocusChanged mWindowFocusChanged false mUpcomingWindowFocus true mAdded true"
+          )
+        end,
+        focus_on_open = false,
+      },
+    })
+    local telescope = require("telescope")
+    local whichKey = require("which-key")
 
-    whichKey.add {
-      { '<leader>F', name = '[F]lutter' },
-    }
+    whichKey.add({
+      { "<leader>F", name = "[F]lutter" },
+    })
 
-    vim.keymap.set('n', '<leader>Fc', telescope.extensions.flutter.commands,
-      { desc = 'Display available flutter commands', noremap = true })
-    vim.keymap.set('n', '<leader>Fr', ':FlutterReload<CR>', { desc = 'Flutter hot [r]eload', noremap = true })
-    vim.keymap.set('n', '<leader>FR', ':FlutterRestart<CR>', { desc = 'Flutter hot [R]estart', noremap = true })
-    vim.keymap.set('n', '<leader>Fq', ':FlutterQuit<CR>', { desc = 'Flutter [q]uit', noremap = true })
+    vim.keymap.set(
+      "n",
+      "<leader>Fc",
+      telescope.extensions.flutter.commands,
+      { desc = "Display available flutter commands", noremap = true }
+    )
+    vim.keymap.set("n", "<leader>Fr", ":FlutterReload<CR>", { desc = "Flutter hot [r]eload", noremap = true })
+    vim.keymap.set("n", "<leader>FR", ":FlutterRestart<CR>", { desc = "Flutter hot [R]estart", noremap = true })
+    vim.keymap.set("n", "<leader>Fq", ":FlutterQuit<CR>", { desc = "Flutter [q]uit", noremap = true })
   end,
 }
