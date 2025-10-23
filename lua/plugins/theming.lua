c309e3d2b0280950750361230708ccefa19715ff
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2025-10-23 23:10:10 +0300

    add log colorizer

--- lua/plugins/theming.lua
+++ lua/plugins/theming.lua
@@ -2,4 +2,15 @@ return {
   {
     "dressing.nvim",
   },
+  {
+    "mtdl9/vim-log-highlighting",
+  },
+  {
+    "chrisbra/Colorizer",
+    config = function()
+      -- Auto-colorize log files
+      vim.g.colorizer_auto_filetype = "log"
+      vim.g.colorizer_disable_bufleave = 1
+    end,
+  },
 }
